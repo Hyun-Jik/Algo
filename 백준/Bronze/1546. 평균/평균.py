66090e53af4a5510bfd60ce15f650cf90cdcbb18
Hyun-Jik <139331395+Hyun-Jik@users.noreply.github.com>
2023-09-21 15:32:15 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,8 @@
 N = int(input())
-arr = list(map(int,input().split()))
-max_score = max(arr)
+mylist = list(map(int,input().split()))
 
-alist = [] # 빈 리스트 만들어주기
-for i in arr:
-    alist.append(i/max_score * 100) # 새로운 계산 값 만들어 주고 리스트로 만들기
-new_avg = sum(alist)/N  # 새로운 계산 값들의 평균 구하기
-print(new_avg)
+# 점수들의 합 *100 / 최대 점수 / 점수 갯수 로 구해야 한다
+mymax=max(mylist)  # 최대 점수
+mysum=sum(mylist)  # 점수들의 합
+
+print(mysum*100/mymax/int(N))
