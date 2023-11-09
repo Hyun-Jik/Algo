bf45cb3507e0d6f3985dd10f3f1e34050bb14a08
Hyun-Jik <139331395+Hyun-Jik@users.noreply.github.com>
2023-11-09 23:00:37 +0900

    [D2] Title: 조교의 성적 매기기, Time: 114 ms, Memory: 45,576 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,16 +1,17 @@
 T = int(input())
-for test_case in range(1, T+1):
+
+for tc in range(1, T+1):
+    grade = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
     N, K = map(int, input().split())
-    grade = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
- 
-    score = []
-    for i in range(N):
-        A, B, C = map(int, input().split())
-        total = (A*0.35)+(B*0.45)+(C*0.2)
-        score.append(total)
- 
+    score = [] # 빈 숫자 리스트
+
+    for _ in range(N): # 첫번째 케이스(10명) 처럼 10명에 대해서
+        M, F, H = map(int,input().split()) # 중간, 기말, 과제 점수가 부여됨
+        total = (M*0.35) + (F*0.45) + (H*0.2) # 각 숫자 들의 총점 구하기
+        score.append(total) # 계산된 총점을 붙여줌 - list로 나타날 것
+
     K_score = score[K-1]
     score.sort(reverse=True)
-    number = score.index(K_score)//(N//10)
- 
-    print(f'#{test_case} {grade[number]}')
+    number = score.index(K_score)
+
+    print(f'#{tc} {grade[number//(N//10)]}')
